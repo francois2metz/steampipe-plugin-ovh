29ee737da1bb4235fa729ae00fa25ed13918bf56
François de Metz <francois@2metz.fr>
2022-03-24 22:41:48 +0100

    Parallelize requests using HydrateDependencies for postgres.

--- ovh/table_ovh_cloud_postgres.go
+++ ovh/table_ovh_cloud_postgres.go
@@ -22,18 +22,21 @@ func tableOvhCloudPostgres() *plugin.Table {
 			KeyColumns: plugin.AllColumns([]string{"project_id", "id"}),
 			Hydrate:    getPostgres,
 		},
+		HydrateDependencies: []plugin.HydrateDependencies{
+			{Func: getProjectInfo},
+		},
 		Columns: []*plugin.Column{
 			{Name: "project_id", Type: proto.ColumnType_STRING, Transform: transform.FromQual("project_id"), Description: "Project id."},
 			{Name: "id", Type: proto.ColumnType_STRING, Description: "Service id."},
-			{Name: "engine", Type: proto.ColumnType_STRING, Description: "Name of the engine of the service."},
-			{Name: "plan", Type: proto.ColumnType_STRING, Description: "Plan of the cluster."},
-			{Name: "created_at", Type: proto.ColumnType_DATETIME, Description: "Date of the creation of the cluster."},
-			{Name: "status", Type: proto.ColumnType_STRING, Description: "Current status of the cluster."},
-			{Name: "node_number", Type: proto.ColumnType_STRING, Description: "Number of nodes in the cluster."},
-			{Name: "description", Type: proto.ColumnType_STRING, Description: "Description of the cluster."},
-			{Name: "version", Type: proto.ColumnType_STRING, Description: "Version of the engine deployed on the cluster."},
-			{Name: "network_type", Type: proto.ColumnType_STRING, Description: "Type of network of the cluster."},
-			{Name: "flavor", Type: proto.ColumnType_STRING, Description: "The VM flavor used for this cluster."},
+			{Name: "engine", Hydrate: getPostgresInfo, Type: proto.ColumnType_STRING, Description: "Name of the engine of the service."},
+			{Name: "plan", Hydrate: getPostgresInfo, Type: proto.ColumnType_STRING, Description: "Plan of the cluster."},
+			{Name: "created_at", Hydrate: getPostgresInfo, Type: proto.ColumnType_DATETIME, Description: "Date of the creation of the cluster."},
+			{Name: "status", Hydrate: getPostgresInfo, Type: proto.ColumnType_STRING, Description: "Current status of the cluster."},
+			{Name: "node_number", Hydrate: getPostgresInfo, Type: proto.ColumnType_STRING, Description: "Number of nodes in the cluster."},
+			{Name: "description", Hydrate: getPostgresInfo, Type: proto.ColumnType_STRING, Description: "Description of the cluster."},
+			{Name: "version", Hydrate: getPostgresInfo, Type: proto.ColumnType_STRING, Description: "Version of the engine deployed on the cluster."},
+			{Name: "network_type", Hydrate: getPostgresInfo, Type: proto.ColumnType_STRING, Description: "Type of network of the cluster."},
+			{Name: "flavor", Hydrate: getPostgresInfo, Type: proto.ColumnType_STRING, Description: "The VM flavor used for this cluster."},
 		},
 	}
 }
@@ -57,6 +60,23 @@ type Postgres struct {
 	Flavor            string            `json:"flavor"`
 }
 
+func getPostgresInfo(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	plugin.Logger(ctx).Trace("getPostgresInfo")
+	postgres := h.Item.(Postgres)
+	projectId := d.KeyColumnQuals["project_id"].GetStringValue()
+
+	client, err := connect(ctx, d)
+	if err != nil {
+		return nil, err
+	}
+
+	err = client.Get(fmt.Sprintf("/cloud/project/%s/database/postgresql/%s", projectId, postgres.ID), &postgres)
+	if err != nil {
+		return nil, err
+	}
+	return postgres, nil
+}
+
 func listPostgres(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
 	client, err := connect(ctx, d)
 	if err != nil {
@@ -70,26 +90,15 @@ func listPostgres(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateDat
 	}
 	for _, postgresId := range postgresIds {
 		var postgres Postgres
-		err = client.Get(fmt.Sprintf("/cloud/project/%s/database/postgresql/%s", projectId, postgresId), &postgres)
-		if err != nil {
-			return nil, err
-		}
+		postgres.ID = postgresId
 		d.StreamListItem(ctx, postgres)
 	}
 	return nil, nil
 }
 
 func getPostgres(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
-	client, err := connect(ctx, d)
-	if err != nil {
-		return nil, err
-	}
-	projectId := d.KeyColumnQuals["project_id"].GetStringValue()
 	id := d.KeyColumnQuals["id"].GetStringValue()
 	var postgres Postgres
-	err = client.Get(fmt.Sprintf("/cloud/project/%s/database/postgresql/%s", projectId, id), &postgres)
-	if err != nil {
-		return nil, err
-	}
+	postgres.ID = id
 	return postgres, nil
 }
