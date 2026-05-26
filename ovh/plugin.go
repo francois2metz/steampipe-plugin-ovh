f8cb8b2268a1cbc070dd1774e312c06cbe86de3a
François de Metz <francois@2metz.fr>
2026-05-26 11:34:02 +0200

    Fix indent.

--- ovh/plugin.go
+++ ovh/plugin.go
@@ -18,7 +18,7 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 		TableMap: map[string]*plugin.Table{
 			"ovh_bill":                  tableOvhBill(),
 			"ovh_bill_detail":           tableOvhBillDetails(),
-			"ovh_ceph":       			 tableOvhCeph(),
+			"ovh_ceph":                  tableOvhCeph(),
 			"ovh_cloud_ai_app":          tableOvhCloudAIApp(),
 			"ovh_cloud_ai_job":          tableOvhCloudAIJob(),
 			"ovh_cloud_ai_notebook":     tableOvhCloudAINotebook(),
